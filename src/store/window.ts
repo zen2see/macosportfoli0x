ec7cbe76deeb5bd29d98514d41440684992fff4a
zen2see <zen2see@gmail.com>
2026-01-30 20:47:23 -0500

    UPDATED right before dock and git checkout

---
+++ src/store/window.ts
@@ -0,0 +1,35 @@
+import { create } from "zustand"
+import { immer } from "zustand/middleware/immer"
+import { WINDOW_CONIG } from "constants/index.ts"
+
+const useWindowStore = create(
+  immer((set) => ({
+    windows: WINDOW_CONFIG,
+    nextZIndex: INITIAL_Z_INDEX + 1,
+
+    openWindow: (windowKey, data = null) =>
+      set((state) => {
+        const win = state.windows[windowKey];
+        win.isOpen = true;
+        win.zIndex = state.nextZIndex;
+        win.data = data ?? win.data;
+        state.nextZIndex++;
+      }),
+
+    closeWindow: (windowKey) =>
+      set((state) => {
+        const win = state.windows[windowKey];
+        win.isOpen = false;
+        win.zIndex = INITIAL_Z_INDEX;
+        win.data = null;
+      }),
+
+    focusWindow: (windowKey) =>
+      set((state) => {
+        const win = state.windows[windowKey];
+        win.zIndex = state.nextZIndex++;
+      }),
+  }))
+);
+
+export default useWindowStore;
