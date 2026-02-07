80d21baabed910bf03f75be8c42b39afa0a1541e
zen2see <zen2see@gmail.com>
2026-02-07 16:09:21 -0500

    UPDATED TERMINAL ICON

--- src/hoc/WindowWrapper.tsx
+++ src/hoc/WindowWrapper.tsx
@@ -1,25 +1,35 @@
 import useWindowStore from '#store/window.ts'
 import { useRef, useLayoutEffect } from 'react'
-import { gsap } from '@gsap/react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { Draggable } from 'gsap/Draggable'
 
 const WindowWrapper = (Component, windowKey) => {
   const Wrapped = (props) => {
     const { focusWindow, windows } = useWindowStore();
     const { isOpen, zIndex } = windows[windowKey] || { isOpen: false, zIndex: 0 };
     const ref = useRef(null);
-    // useGSAP(() => {
-    //   const el = ref.current
-    //   if (!el || !isOpen) return;
-    //   el.style.display = "block";
-    //   gsap.fromTo(el, { scale: 0.8, opacity: 0, y: 40 }, 
-    //   { scale: 1, opacity: 1, y: 0, duration: 0.4, ease: "power3.out" });
-    // }, [isOpen]);
+
+    useGSAP(() => {
+      const el = ref.current
+      if (!el || !isOpen) return;
+      el.style.display = "block";
+      gsap.fromTo(el, { scale: 0.8, opacity: 0, y: 40 }, 
+      { scale: 1, opacity: 1, y: 0, duration: 0.4, ease: "power3.out" });
+    }, [isOpen]);
+
+    useGSAP(() => {
+      const el = ref.current;
+      if (!el) return;
+      const [instance] = Draggable.create(el, { onPress: () => focusWindow(windowKey)});
+      return () => instance.kill();
+    }, []);
    
-    // useLayoutEffect(() => {
-    //   const el = ref.current
-    //   if (!el) return;
-    //   el.style.display = isOpen ? "block" : "none";
-    // }, [isOpen]);
+    useLayoutEffect(() => {
+      const el = ref.current
+      if (!el) return;
+      el.style.display = isOpen ? "block" : "none";
+    }, [isOpen]);
 
     return (
       <section id={windowKey} ref={ref} style={{ zIndex }} className="absolute" >
--- src/windows/Terminal.tsx
+++ src/windows/Terminal.tsx
@@ -1,6 +1,6 @@
-// import WindowWrapper from "#hoc/WindowWrapper";
-// import { Check, Flag } from 'lucide-react';
-// import { techStack } from "#constants/index.tsx";
+import WindowWrapper from "#hoc/WindowWrapper";
+import { Check, Flag } from 'lucide-react';
+import { techStack } from "#constants/index.tsx";
 
 const Terminal = () => {
   return (
@@ -14,42 +14,48 @@ const Terminal = () => {
           <span className="font-bold">@ZMorris % </span> 
             show tech Stack
         </p>
+        <div className="label">
+          <p className="w-32">Category</p>
+          <p>Technologies</p>
+        </div> 
+        <ul className="content">
+          {techStack.map(({ category, items }) => (
+            <li key={category} className="flex items-center">
+              <Check className="check" size={20} />
+              <h3>{category}</h3>
+              <ul>
+               {items.map((item, i) => (
+                 <li key={i}>
+                   {item} 
+                   {i < items.length - 1 ? "," : ""}
+                 </li>
+                 ))}
+              </ul>
+            </li>  
+          ))}
+        </ul>
+        <div className="footnote">
+          <p>
+            <Check size={20} /> 5 of 5 stacks loaded successfully 
+             (100%)
+          </p>
+          <p className="text-black">
+            <Flag size={15} fill="black" />
+            Render time: 6ms
+          </p>
+        </div>
       </div>  
+    
     </>
   )
 }  
 
-export default Terminal;
-// 
+const TerminalWindow = WindowWrapper(Terminal, 'terminal')
+export default TerminalWindow;
 
 
-    //     <div className="label">
-    //       <p className="w-32">Category</p>
-    //       <p>Technologies</p>
-    //     </div>
-      //   <ul className="content">
-      //     {techStack.map(({ category, items }) => (
-      //       <li key={category} className="flex items-center">
-      //         <Check className="check" size={20} />
-      //         <h3>{category}</h3>
-      //         <ul>
-      //           {items.map((item, i) => (
-      //             <li key={i}>
-      //               {item} 
-      //               {i < items.length - 1 ? "," : ""}
-      //             </li>
-      //           ))}
-      //       </ul>
-      //     </li>   
-      //   ))}  
-      //   </ul>
-      //   <div className="footnote">
-      //     <p>
-      //       <Check size={20} /> 5 of 5 stacks loaded successfully 
-      //        (100%)
-      //     </p>
-      //     <p className="text-black">
-      //       <Flag size={15} fill="black" />
-      //       Render time: 6ms
-      //     </p>
-      //   </div>
+        
+  
+        
+        
+        
